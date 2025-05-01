0d72ebf8a56ca7dc8703844eec27dc1a065ffc24
Jaison Vieira <jaisonnvieira@gmail.com>
2025-04-30 21:00:18 -0500

    Clean up main.go

--- main.go
+++ main.go
@@ -6,22 +6,32 @@ import (
 )
 
 const (
-	filepathRoot = "."
-	filepathLogo = "./assets/"
-	port         = ":8080"
+	filepathRoot   = "./"
+	filepathAssets = "./assets"
+	port           = "8080"
 )
 
 func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	// mux.Handle("/assets", http.FileServer(http.Dir(filepathAssets)))
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	// mux.Handle("/app/assets", http.StripPrefix("/app", http.FileServer(http.Dir(filepathAssets))))
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := &http.Server{
 		Handler: mux,
-		Addr:    port,
+		Addr:    ":" + port,
 	}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
